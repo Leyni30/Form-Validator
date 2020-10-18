990530e40e569e9b991417f1e02a278c3e7fb8a3
Elena <elena_donceva@yahoo.com>
2020-10-18 17:29:56 -0400

    I deleted all the if statements and wrote my first function that uses another inside it; the first one ischecking if the user input is an empty string or they've written something. If not it calls the error function previously  created.

--- script.js
+++ script.js
@@ -21,31 +21,23 @@ function isValidEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+
+function checkRequired(inputArr) {
+    inputArr.forEach(input => {
+        if (input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    })
+}
+
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+// Event listener
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     //preventing the form to be by default submited to the file
-    //then I go on to check for conditions
-    if (username.value === '') {
-        showError(username, 'Username is required');
-    } else {
-        showSuccess(username);
-    }
-    if (email.value === '') {
-        showError(email, 'Email is required');
-    } else if (!isValidEmail(email.value)) {
-        showError(email, 'Email is not valid');
-    } else {
-        showSuccess(email);
-    }
-    if (password.value === '') {
-        showError(password, 'Password is required');
-    } else {
-        showSuccess(password);
-    }
-    if (password2.value === '') {
-        showError(password2, 'Password 2is required');
-    } else {
-        showSuccess(password2);
-    }
-
+    checkRequired([username, email, password, password2])
 })
